d427aac24304ef202cbb319c4d99a2440f8a0291
bubkoo <bubkoo.wy@gmail.com>
2022-07-05 15:26:54 +0800

    refactor: use github-script to replace local actions

---
+++ scripts/coder-calendar.js
@@ -0,0 +1,46 @@
+function padDateNumber(m) {
+  return m > 9 ? `${m}` : `0${m}`
+}
+
+module.exports = async ({ github, context, core, generateSVG }) => {
+  const now = new Date()
+  const yyyy = now.getFullYear()
+  const mm = padDateNumber(now.getMonth() + 1)
+  const dd = padDateNumber(now.getDate())
+
+  const filename = `${yyyy}-${mm}-${dd}.svg`
+  const filepath = `assets/coder-calendar/${filename}`
+  const content = generateSVG('bubkoo')
+
+  const getContent = async () => {
+    try {
+      return await github.rest.repos.getContent({
+        ...context.repo,
+        path: filepath,
+      })
+    } catch (e) {
+      return null
+    }
+  }
+
+  const res = await getContent(github, filepath)
+  const oldContent = res
+    ? Buffer.from(res.data.content, 'base64').toString()
+    : null
+  if (oldContent !== content) {
+    await github.rest.repos.createOrUpdateFileContents({
+      ...context.repo,
+      path: filepath,
+      content: Buffer.from(content).toString('base64'),
+      message: `chore: ${
+        oldContent != null ? 'update' : 'generate'
+      } daily coder-calender [skip ci]`,
+      sha: res ? res.data.sha : undefined,
+    })
+    core.info(`File "${filepath}" ${oldContent ? 'updated' : 'generated'}`)
+  } else {
+    core.info(`File "${filepath}" no need to update`)
+  }
+
+  return filepath
+}
