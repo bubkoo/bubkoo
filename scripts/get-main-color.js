4607c96518e4ec5d7301edb315397759b45f7ba2
bubkoo <bubkoo.wy@gmail.com>
2022-07-12 16:15:18 +0800

    feat: extra filename from content-type

--- scripts/get-main-color.js
+++ scripts/get-main-color.js
@@ -1,15 +1,28 @@
 const os = require('os')
 const fs = require('fs')
+const util = require('util')
 const path = require('path')
+const mime = require('mime-types')
 const fetch = require('node-fetch')
 const { extractColors } = require('extract-colors')
+const streamPipeline = util.promisify(require('stream').pipeline)
+
 
 module.exports = async ({ github, context, core, url }) => {
   try {
-    const filename = path.basename(url) || `${new Date().getTime()}.png`
-    const filepath = path.join(os.tmpdir(), filename)
     const res = await fetch(url)
-    res.body.pipe(fs.createWriteStream(filepath))
+    if (!res.ok) {
+      throw new Error(`unexpected response ${res.statusText}`)
+    }
+
+    let filename = path.basename(url)
+    if ((/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i).test(filename)) {
+      const type = res.headers.get('Content-Type')
+      const ext = type ? mime.extension(type) || 'png' : 'png'
+      filename = `${new Date().getTime()}.${ext}`
+    }
+    const filepath = path.join(os.tmpdir(), filename)
+    await streamPipeline(res.body, fs.createWriteStream(filepath))
     const colors = extractColors(filepath)
     colors.sort((a, b) => a.area - b.area)
     core.info(JSON.stringify(colors, null, 2))
