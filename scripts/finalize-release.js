ae952789aa48b3d67d3e4684302539f2c1b98093
崖 <bubkoo.wy@gmail.com>
2022-11-04 21:05:15 +0800

    Update finalize-release.js

--- scripts/finalize-release.js
+++ scripts/finalize-release.js
@@ -1,13 +1,28 @@
 module.exports = async ({ github, context, core }) => {
-  console.log(context,github)
-  const path = '.releasing'
-  const res = await github.rest.repos.getContent({
-      ...context.repo,
-      path,
-      ref: context.ref,
-    })
+  console.log(context)
 
-  if(res){
+  const { prs } = await github.rest.repos.listPullRequestsAssociatedWithCommit({
+    ...context.repo,
+    commit_sha: context.sha,
+  });
+
+  const pr =
+    prs.find((item) =>
+      context.payload.ref === `refs/heads/${item.head.ref}`
+    ) || prs[0];
+
+  if (pr) {
+    const path = '.releasing'
+    let res
+    try {
+      res = await github.rest.repos.getContent({
+        ...context.repo,
+        path,
+        ref: context.ref,
+      })
+    } catch (e) {}
+
+    if (res) {
       await github.rest.repos.deleteFile({
         ...context.repo,
         path,
@@ -15,12 +30,12 @@ module.exports = async ({ github, context, core }) => {
         sha: res.data.sha,
         branch: context.ref,
       })
+    }
+
+    await github.rest.pulls.merge({
+      ...context.repo,
+      pull_number: pr.number,
+      merge_method: 'squash',
+    })
   }
-
-
-  await github.rest.pulls.merge({
-    ...context.repo,
-    pull_number: context.payload.pull_request.number,
-    merge_method: 'squash',
-  })
 }
